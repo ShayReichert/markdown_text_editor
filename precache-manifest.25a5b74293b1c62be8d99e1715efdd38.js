5a31e8bc5880c660c44e774f6ca9d53fcbd8a32f
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-07-27 12:03:02 +0200

    Updates

---
+++ precache-manifest.25a5b74293b1c62be8d99e1715efdd38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd23720c01b909b6e919664c0c03dbb",
+    "url": "/markdown_text_editor/index.html"
+  },
+  {
+    "revision": "633601a5cb4cdb7ca6cb",
+    "url": "/markdown_text_editor/static/css/main.a2159767.chunk.css"
+  },
+  {
+    "revision": "9aa515266499ad019bcb",
+    "url": "/markdown_text_editor/static/js/2.c1e2e18c.chunk.js"
+  },
+  {
+    "revision": "cbbbb0b9131a638ba1fa969274b2219c",
+    "url": "/markdown_text_editor/static/js/2.c1e2e18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633601a5cb4cdb7ca6cb",
+    "url": "/markdown_text_editor/static/js/main.fa45e97f.chunk.js"
+  },
+  {
+    "revision": "9208563a64f05bf48f34",
+    "url": "/markdown_text_editor/static/js/runtime-main.22f97e9a.js"
+  }
+]);
